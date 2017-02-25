67470220f621c90af974400ac55fe12ecf5b304a
liaronce <1062787843@qq.com>
2017-02-25 14:01:59 +0800

    Upload plugin.
    Build the plugin and delete the wix installer file.

--- MarkdownInLiveWriter/MarkdownForm.Designer.cs
+++ MarkdownInLiveWriter/MarkdownForm.Designer.cs
@@ -52,11 +52,11 @@
             this.markdownTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.markdownTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.markdownTextBox.Location = new System.Drawing.Point(12, 35);
+            this.markdownTextBox.Location = new System.Drawing.Point(12, 32);
             this.markdownTextBox.Multiline = true;
             this.markdownTextBox.Name = "markdownTextBox";
             this.markdownTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.markdownTextBox.Size = new System.Drawing.Size(509, 683);
+            this.markdownTextBox.Size = new System.Drawing.Size(509, 631);
             this.markdownTextBox.TabIndex = 0;
             // 
             // htmlTabControl
@@ -67,10 +67,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.htmlTabControl.Controls.Add(this.PreviewPage);
             this.htmlTabControl.Controls.Add(this.SourcePage);
-            this.htmlTabControl.Location = new System.Drawing.Point(527, 35);
+            this.htmlTabControl.Location = new System.Drawing.Point(527, 32);
             this.htmlTabControl.Name = "htmlTabControl";
             this.htmlTabControl.SelectedIndex = 0;
-            this.htmlTabControl.Size = new System.Drawing.Size(469, 683);
+            this.htmlTabControl.Size = new System.Drawing.Size(469, 630);
             this.htmlTabControl.TabIndex = 1;
             // 
             // PreviewPage
@@ -79,7 +79,7 @@
             this.PreviewPage.Location = new System.Drawing.Point(4, 4);
             this.PreviewPage.Name = "PreviewPage";
             this.PreviewPage.Padding = new System.Windows.Forms.Padding(3);
-            this.PreviewPage.Size = new System.Drawing.Size(461, 657);
+            this.PreviewPage.Size = new System.Drawing.Size(461, 604);
             this.PreviewPage.TabIndex = 0;
             this.PreviewPage.Text = "Preview";
             this.PreviewPage.UseVisualStyleBackColor = true;
@@ -88,9 +88,9 @@
             // 
             this.previewWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
             this.previewWebBrowser.Location = new System.Drawing.Point(3, 3);
-            this.previewWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.previewWebBrowser.MinimumSize = new System.Drawing.Size(20, 18);
             this.previewWebBrowser.Name = "previewWebBrowser";
-            this.previewWebBrowser.Size = new System.Drawing.Size(455, 651);
+            this.previewWebBrowser.Size = new System.Drawing.Size(455, 598);
             this.previewWebBrowser.TabIndex = 0;
             // 
             // SourcePage
@@ -99,7 +99,7 @@
             this.SourcePage.Location = new System.Drawing.Point(4, 4);
             this.SourcePage.Name = "SourcePage";
             this.SourcePage.Padding = new System.Windows.Forms.Padding(3);
-            this.SourcePage.Size = new System.Drawing.Size(461, 657);
+            this.SourcePage.Size = new System.Drawing.Size(461, 604);
             this.SourcePage.TabIndex = 1;
             this.SourcePage.Text = "Source";
             this.SourcePage.UseVisualStyleBackColor = true;
@@ -111,14 +111,14 @@
             this.sourceTextBox.Multiline = true;
             this.sourceTextBox.Name = "sourceTextBox";
             this.sourceTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.sourceTextBox.Size = new System.Drawing.Size(455, 651);
+            this.sourceTextBox.Size = new System.Drawing.Size(455, 598);
             this.sourceTextBox.TabIndex = 0;
             // 
             // insertBtn
             // 
             this.insertBtn.Location = new System.Drawing.Point(12, 5);
             this.insertBtn.Name = "insertBtn";
-            this.insertBtn.Size = new System.Drawing.Size(75, 23);
+            this.insertBtn.Size = new System.Drawing.Size(75, 21);
             this.insertBtn.TabIndex = 2;
             this.insertBtn.Text = "Insert";
             this.insertBtn.UseVisualStyleBackColor = true;
@@ -128,7 +128,7 @@
             // 
             this.highlightBtn.Location = new System.Drawing.Point(914, 6);
             this.highlightBtn.Name = "highlightBtn";
-            this.highlightBtn.Size = new System.Drawing.Size(75, 23);
+            this.highlightBtn.Size = new System.Drawing.Size(75, 21);
             this.highlightBtn.TabIndex = 3;
             this.highlightBtn.Text = "Highlight";
             this.highlightBtn.UseVisualStyleBackColor = true;
@@ -142,7 +142,7 @@
             this.highlightCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
             this.highlightCheckbox.Location = new System.Drawing.Point(93, 5);
             this.highlightCheckbox.Name = "highlightCheckbox";
-            this.highlightCheckbox.Size = new System.Drawing.Size(101, 17);
+            this.highlightCheckbox.Size = new System.Drawing.Size(114, 16);
             this.highlightCheckbox.TabIndex = 4;
             this.highlightCheckbox.Text = "Highlight Code?";
             this.highlightCheckbox.UseVisualStyleBackColor = true;
@@ -150,9 +150,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Location = new System.Drawing.Point(200, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 5;
             this.label1.Text = "Language:";
             // 
@@ -161,24 +161,24 @@
             this.languageComboBox.FormattingEnabled = true;
             this.languageComboBox.Location = new System.Drawing.Point(264, 5);
             this.languageComboBox.Name = "languageComboBox";
-            this.languageComboBox.Size = new System.Drawing.Size(257, 21);
+            this.languageComboBox.Size = new System.Drawing.Size(257, 20);
             this.languageComboBox.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(528, 11);
+            this.label2.Location = new System.Drawing.Point(528, 10);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 7;
             this.label2.Text = "Style:";
             // 
             // styleComboBox
             // 
             this.styleComboBox.FormattingEnabled = true;
-            this.styleComboBox.Location = new System.Drawing.Point(567, 7);
+            this.styleComboBox.Location = new System.Drawing.Point(567, 6);
             this.styleComboBox.Name = "styleComboBox";
-            this.styleComboBox.Size = new System.Drawing.Size(205, 21);
+            this.styleComboBox.Size = new System.Drawing.Size(205, 20);
             this.styleComboBox.TabIndex = 8;
             // 
             // showLineCheckBox
@@ -187,18 +187,18 @@
             this.showLineCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.showLineCheckBox.Checked = true;
             this.showLineCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.showLineCheckBox.Location = new System.Drawing.Point(778, 8);
+            this.showLineCheckBox.Location = new System.Drawing.Point(778, 7);
             this.showLineCheckBox.Name = "showLineCheckBox";
-            this.showLineCheckBox.Size = new System.Drawing.Size(116, 17);
+            this.showLineCheckBox.Size = new System.Drawing.Size(120, 16);
             this.showLineCheckBox.TabIndex = 9;
             this.showLineCheckBox.Text = "Show Line Number";
             this.showLineCheckBox.UseVisualStyleBackColor = true;
             // 
             // MarkdownForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.ClientSize = new System.Drawing.Size(1008, 674);
             this.Controls.Add(this.showLineCheckBox);
             this.Controls.Add(this.styleComboBox);
             this.Controls.Add(this.label2);
@@ -210,7 +210,7 @@
             this.Controls.Add(this.htmlTabControl);
             this.Controls.Add(this.markdownTextBox);
             this.Name = "MarkdownForm";
-            this.Text = "Markdown In Windows Live Writer";
+            this.Text = "Markdown In Open Live Writer";
             this.htmlTabControl.ResumeLayout(false);
             this.PreviewPage.ResumeLayout(false);
             this.SourcePage.ResumeLayout(false);
--- README.md
+++ README.md
@@ -1,54 +1,37 @@
 MarkdownInLiveWriter
 ====================
 
-This project is a markdown plugin for [Windows Live Writer](http://windows.microsoft.com/en-us/windows-live/essentials-other#essentials=overviewother). You can use this plugin to write your blog in markdown, and it provides the live preview and code syntax highlight.
+This project is a markdown plugin for [Open Live Writer](http://openlivewriter.org). 
+
+You can use this plugin to write your blog in markdown, and it provides the live preview and code syntax highlight.
+
+It built from [fresky/MarkdownInLiveWriter](https://github.com/fresky/MarkdownInLiveWriter)
 
 ## Installation
 
 **Option 1:**
 
-Download the [MarkdownInLiveWriter.dll](https://github.com/fresky/MarkdownInLiveWriter/raw/master/MarkdownInLiveWriter.dll), 
-then put it in the [WindowsLiveWriterPath]\Plugins\.
+Download the [MarkdownInLiveWriter.dll](https://github.com/LiarOnce/MarkdownInLiveWriter/raw/master/MarkdownInLiveWriter.dll), 
+then put it in the [OpenLiveWriterPath]\Plugins\.
 
-**Option 2:**
+## Screenshot
 
-Download the [MarkdownInLiveWriter.msi](https://github.com/fresky/MarkdownInLiveWriter/raw/master/MarkdownInLiveWriter.msi), then run the installer. 
-
-## Usage
-
-Please refer to the below screen shot.
-![screenshot](https://raw.github.com/fresky/MarkdownInLiveWriter/master/screenshot.png "MarkdownInLiveWriter")
-
-1. Click the **Insert** menu in Windows Live Writer, then click the **Insert Markdown**.
-2. Write your blog in the left panel with Markdown, you can see the live preview in the right Panel.
-3. You can also click the **source** tab to see the html source for the markdown.
-4. You can specify the programming language and display style.
-5. Click the "Highlight" button to preview the code highlight.
-5. Click the **Insert** button, then the content will be inserted to the Windows Live Writer.
+![screenshot](https://ooo.0o0.ooo/2017/02/25/58b11c88ef3fb.png)
 
 ## Requirements
 
-Please install [Windows Live Writer](http://windows.microsoft.com/en-us/windows-live/essentials-other#essentials=overviewother)
-
-## Build Installer
-Install [Wixtoolset](http://wixtoolset.org/). Then run the following 2 commands:  
-
-    Wixpath\candle.exe -out markdowninlivewriter.wixobj MarkdownInLiveWriter.wxs   
-    Wixpath\light.exe -ext WixUIExtension MarkdownInLiveWriter.wixobj
-
+Please install [Open Live Writer](http://openlivewriter.org)
 
 ## Credits
 
 This project is using [markdownsharp](http://code.google.com/p/markdownsharp/), which is a open source C# implementation of Markdown processor.
-
-And the wix file is base on the [Mike Stall's post](http://blogs.msdn.com/b/jmstall/archive/2007/10/27/wix-script-for-installing-live-writer-plugins.aspx).
 
 ## License
 
 MarkdownInLiveWriter is released under the MIT License. See the bundled LICENSE file for details.
 
 ## Chang Log
-
+1. 02/25/2017	build the Open Live Writer ver. and delete the wix installer file
 1. 08/01/2013	add the wix installer file
 1. 07/19/2013	support code syntax highlight
 1. 07/16/2013	initial version
